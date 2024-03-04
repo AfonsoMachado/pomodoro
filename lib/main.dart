475b0d38270ba00475367848cde0e3a97cee5db8
Afonso Machado <afonsosmachado@gmail.com>
2024-03-04 09:36:23 -0300

    create new main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:pomodoro/store/counter.store.dart';
+import 'package:pomodoro/components/pomodoro.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,50 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pomodoro',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const HomePage(),
-    );
-  }
-}
-
-final store = CounterStore();
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Counter'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            // Permite atualizar os dados
-            Observer(
-              builder: (_) => Text(
-                '${store.counter}',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: store.increment,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const Pomodoro(),
     );
   }
 }
